0387ecef24436f24ba53c0d255b95774a05984c4
joe <joeho7576@gmail.com>
2023-07-09 05:32:52 +0330

    change code, think in very basic terms

--- Part 1/Arrays/arrayClass.py
+++ Part 1/Arrays/arrayClass.py
@@ -1,5 +1,5 @@
 # how should i implement array class in python? bruh. python is like water :D
-
+# try to think very basic!
 # class should be initialize with fixed size, have insert, removeAt, indexOf, print methods
 
 import array # -> wow!
@@ -15,50 +15,72 @@ class Array():
             # add to array when array is not full
             if not self.array[i]:
                 self.array[i] = item
-                return
-            
-            # extend array one cell to append item to array
-            elif i == self.len_array - 1:
-                self.array += ['']
-                self.array[-1] = item
-                self.len_array += 1
+                return # TODO: user return or break?
+        
+        # extend array 
+        new_array = [''] * ( 2 * self.len_array)
+        for i in range(len(new_array)):
+            if i < self.len_array:
+                new_array[i] = self.array[i]
+            else:
+                new_array[i] = item
+                self.array = new_array
+                self.len_array = len(self.array)
                 return
             
     def removeAt(self, index):
-        # index bigger than array length is meaningless
-        if index > self.len_array - 1: raise Exception('Index out of range!')
+        # validate index
+        if index >= self.len_array or index < 0: raise Exception('Index out of range!')
         
-        for i in range(self.len_array):
-            if i == index:
-                del self.array[index]
-                self.len_array -= 1
-                return
+        # shift to left
+        # 1 2 3 4 5
+        # 1 3 4 5 -
+        # from 1 to 3
+        for i in range(index, self.len_array - 1): # len array minus one is important
+            self.array[i] = self.array[i + 1]
+        self.len_array -= 1
                 
     def indexOf(self, item):
         for i in range(self.len_array):
             if self.array[i] == item:
                 print(i)
-                return
+                return i
         print(-1)
-        return
+        return -1
     
     def print_array(self):
         for i in range(self.len_array):
-            if self.array[i]:
+            if self.array[i]: #? is this really a cheat?
                 print(self.array[i])
         print('---')
         
             
+# testing
 test_array = Array(length=3)
+
 test_array.insert(item=10)
 test_array.insert(item=20)
 test_array.print_array()
+
 test_array.insert(item=30)
 test_array.insert(item=40)
 test_array.print_array()
+
 test_array.removeAt(index=3)
 test_array.print_array()
-test_array.indexOf(item=20)
-test_array.indexOf(item=100)
 
+test_array.insert(50)
+test_array.insert(60)
+test_array.insert(70)
+test_array.print_array()
 
+test_array.removeAt(3)
+test_array.print_array()
+
+test_array.removeAt(0)
+test_array.print_array()
+
+test_array.indexOf(item=10)
+
+# test_array.indexOf(item=20)
+# test_array.removeAt(index=100)
